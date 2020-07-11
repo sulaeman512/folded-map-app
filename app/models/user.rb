4d015eaa90bdddde008df34589dc09a68424bcf5
carlhauck <carlhauck@gmail.com>
2020-07-11 15:37:44 -0500

    fixes user to conversation association methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
 
   belongs_to :block
-  has_many :conversations
+  has_many :conversations, class_name: "Conversation", foreign_key: "sender_id"
+  has_many :conversations, class_name: "Conversation", foreign_key: "recipient_id"
   has_many :messages
   has_many :posts
   has_many :comments
