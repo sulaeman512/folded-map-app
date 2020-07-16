520f6bfe7e86b00045b5866f1ae3ff389ec5923e
carlhauck <carlhauck@gmail.com>
2020-07-16 00:17:45 -0500

    updates user model methods so that when a user updates their address, conversations are automatically created between them and all other users in their same block and block pair; also adds conversations validation so a new convo can't be created if one exists between two users

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,9 +1,13 @@
 class Conversation < ApplicationRecord
 
-  belongs_to :sender, class_name: "User"
-  belongs_to :recipient, class_name: "User"
+  belongs_to :sender, foreign_key: :sender_id, class_name: "User"
+  belongs_to :recipient, foreign_key: :recipient_id, class_name: "User"
   has_many :messages
 
-  # Add validation that doesn't allow creation of conversation between same two users (if a conversation has already been created)
+  validates_uniqueness_of :sender_id, scope: :recipient_id
+
+  scope :between, -> (sender_id, recipient_id) do
+    where("(conversations.sender_id = ? AND conversations.recipient_id = ?) OR (conversations.sender_id = ? AND conversations.recipient_id = ?)", sender_id, recipient_id, recipient_id, sender_id)
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,137 +22,13 @@ class User < ApplicationRecord
     first_name + " " + last_name[0]
   end
 
-  def self.check_block(block_key, boundary_coordinates)
-    @block_matches = []
-    if @latitude.between?(boundary_coordinates[0][0], boundary_coordinates[1][0]) && @longitude.between?(boundary_coordinates[0][1], boundary_coordinates[1][1])
-      @block_matches << block_key
-    end
-    if @block_matches.length == 1
-      block_match = Block.find_by(name: @block_matches[0])
-      @user.update(
-        block_id: block_match.id || @user.block_id
-      )
-    elsif @block_matches.length > 1
-      block_1 = @block_matches[0].to_s.split("_")
-      block_2 = @block_matches[1].to_s.split("_")
-      if block_1[0] == "nw" && block_2[0] == "nw"
-        if street_direction.downcase == "n"
-          if street_num.to_i.even?
-            if block_1[1] > block_2[1]
-              block_match = Block.where("name = ?", @block_matches[0])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            else
-              block_match = Block.where("name = ?", @block_matches[1])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            end
-          else
-            if block_1[1] < block_2[1]
-              block_match = Block.where("name = ?", @block_matches[0])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            else
-              block_match = Block.where("name = ?", @block_matches[1])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            end
-          end
-        elsif street_direction.downcase == "w"
-          if street_num.to_i.even?
-            if block_1[2] > block_2[2]
-              block_match = Block.where("name = ?", @block_matches[0])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            else
-              block_match = Block.where("name = ?", @block_matches[1])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            end
-          else
-            if block_1[2] < block_2[2]
-              block_match = Block.where("name = ?", @block_matches[0])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            else
-              block_match = Block.where("name = ?", @block_matches[1])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            end
-          end
-        end
-      elsif block_1[0] == "sw" && block_2[0] == "sw"
-        if street_direction.downcase == "s"
-          if street_num.to_i.even?
-            if block_1[1] > block_2[1]
-              block_match = Block.where("name = ?", @block_matches[0])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            else
-              block_match = Block.where("name = ?", @block_matches[1])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            end
-          else
-            if block_1[1] < block_2[1]
-              block_match = Block.where("name = ?", @block_matches[0])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            else
-              block_match = Block.where("name = ?", @block_matches[1])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            end
-          end
-        elsif street_direction.downcase == "w"
-          if street_num.to_i.even?
-            if block_1[2] > block_2[2]
-              block_match = Block.where("name = ?", @block_matches[1])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            else
-              block_match = Block.where("name = ?", @block_matches[0])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            end
-          else
-            if block_1[2] < block_2[2]
-              block_match = Block.where("name = ?", @block_matches[1])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            else
-              block_match = Block.where("name = ?", @block_matches[0])
-              @user.update(
-                block_id: block_match || @user.block_id
-              )
-            end
-          end
-        end
-      end
-    end
-  end
-
   require 'http'
 
-  def self.match_address(user, street_num, street_direction, street, zip_code)
+  # Runs within user controller update action if address params are provided; sends user input to HERE geocoding API, retrieves lat/lng, and standardizes said input for DB storage
+  def self.match_address(user, user_street_num, user_street_direction, user_street, user_zip_code)
     
+    user_street = user_street.split(" ").join("+")
     @user = user
-
     @blocks = {
       nw_8_7: [[41.92473, -87.7074], [41.9321, -87.69743]],
       nw_8_8: [[41.93203, -87.70762], [41.93939, -87.6976]],
@@ -167,23 +43,177 @@ class User < ApplicationRecord
       sw_9_8: [[41.82244, -87.714365], [41.83017, -87.70443]],
       sw_9_9: [[41.815315, -87.7142025], [41.82259, -87.70425]]
     }
-    
-    street = street.split(" ").join("+")
-    request = HTTP.get("https://geocode.search.hereapi.com/v1/geocode?q=#{street_num}+#{street_direction}+#{street}+#{zip_code}&apiKey=#{Rails.application.credentials.here_api[:api_key]}")
+
+    request = HTTP.get("https://geocode.search.hereapi.com/v1/geocode?q=#{user_street_num}+#{user_street_direction}+#{user_street}+#{user_zip_code}&apiKey=#{Rails.application.credentials.here_api[:api_key]}")
+
     @latitude = request.parse["items"][0]["position"]["lat"]
     @longitude = request.parse["items"][0]["position"]["lng"]
-    house_number = request.parse["items"][0]["address"]["houseNumber"]
-    direction = request.parse["items"][0]["address"]["street"][0..0] #grabs first character
+    @house_number = request.parse["items"][0]["address"]["houseNumber"]
+    @direction = request.parse["items"][0]["address"]["street"][0..0] #grabs first character
     street = request.parse["items"][0]["address"]["street"][2..-1] #shaves off direction and space
     postal_code = request.parse["items"][0]["address"]["postalCode"][0..4]
+    
     @user.update(
-      street_num: house_number || @user.street_num,
-      street_direction: direction || @user.street_direction,
+      street_num: @house_number || @user.street_num,
+      street_direction: @direction || @user.street_direction,
       street: street || @user.street,
       zip_code: postal_code || @user.zip_code
     )
+
+    @block_matches = []
     @blocks.each do |k, v|
-      check_block(k, v)
+      find_block_match(k, v)
+    end
+
+    assign_block_match
+
+    if @user.block_id != nil
+      create_block_pair_conversations
+    end
+
+  end
+
+  # Triggered by match_address; looks for block match(es)
+  def self.find_block_match(block_key, boundary_coordinates)
+    if @latitude.between?(boundary_coordinates[0][0], boundary_coordinates[1][0]) && @longitude.between?(boundary_coordinates[0][1], boundary_coordinates[1][1])
+      @block_matches << block_key
+    end
+  end
+
+  # Triggered by match_address; if more than one match, resolves overlapping match
+  def self.assign_block_match
+    if @block_matches.length == 1
+      @block_match = Block.find_by(name: @block_matches[0])
+      @user.update(
+        block_id: @block_match.id || @user.block_id
+      )
+    elsif @block_matches.length > 1
+      block1 = @block_matches[0].to_s.split("_")
+      block2 = @block_matches[1].to_s.split("_")
+      if block1[0] == "nw" && block2[0] == "nw"
+        if @direction.downcase == "n"
+          if @house_number.to_i.even?
+            if block1[1] > block2[1]
+              @block_match = Block.where("name = ?", @block_matches[0])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            else
+              @block_match = Block.where("name = ?", @block_matches[1])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            end
+          else
+            if block1[1] < block2[1]
+              @block_match = Block.where("name = ?", @block_matches[0])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            else
+              @block_match = Block.where("name = ?", @block_matches[1])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            end
+          end
+        elsif @direction.downcase == "w"
+          if @house_number.to_i.even?
+            if block1[2] > block2[2]
+              @block_match = Block.where("name = ?", @block_matches[0])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            else
+              @block_match = Block.where("name = ?", @block_matches[1])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            end
+          else
+            if block1[2] < block2[2]
+              @block_match = Block.where("name = ?", @block_matches[0])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            else
+              @block_match = Block.where("name = ?", @block_matches[1])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            end
+          end
+        end
+      elsif block1[0] == "sw" && block2[0] == "sw"
+        if @direction.downcase == "s"
+          if @house_number.to_i.even?
+            if block1[1] > block2[1]
+              @block_match = Block.where("name = ?", @block_matches[0])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            else
+              @block_match = Block.where("name = ?", @block_matches[1])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            end
+          else
+            if block1[1] < block2[1]
+              @block_match = Block.where("name = ?", @block_matches[0])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            else
+              @block_match = Block.where("name = ?", @block_matches[1])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            end
+          end
+        elsif @direction.downcase == "w"
+          if @house_number.to_i.even?
+            if block1[2] > block2[2]
+              @block_match = Block.where("name = ?", @block_matches[1])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            else
+              @block_match = Block.where("name = ?", @block_matches[0])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            end
+          else
+            if block1[2] < block2[2]
+              @block_match = Block.where("name = ?", @block_matches[1])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            else
+              @block_match = Block.where("name = ?", @block_matches[0])
+              @user.update(
+                block_id: @block_match.id || @user.block_id
+              )
+            end
+          end
+        end
+      end
+    end
+  end
+
+  # Triggered by match_address (if block and block_pair exist); creates conversations between new user and all existing users in same block and block pair
+  def self.create_block_pair_conversations
+    current_user = @user
+    block_pair = @block_match.block_pair
+    block_pair.users.each do |user|
+      if user.id != current_user.id
+        conversation = Conversation.create(
+          sender_id: current_user.id,
+          recipient_id: user.id,
+          map_twin: false
+        )
+      end
     end
   end
 
