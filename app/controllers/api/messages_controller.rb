4ea9f6d04c05f265001e6af6a005eaa6a0a40b03
carlhauck <carlhauck@gmail.com>
2020-08-04 01:18:19 -0500

    adds messaging web socket functionality on backend

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -3,6 +3,7 @@ class Api::MessagesController < ApplicationController
   before_action :authenticate_user
 
   def create
+    @user = current_user
     @conversation = Conversation.find_by(id: params[:conversation_id])
     if @conversation.sender_id == current_user.id || @conversation.recipient_id == current_user.id
       @message = Message.new(
@@ -11,6 +12,13 @@ class Api::MessagesController < ApplicationController
         user_id: current_user.id
       )
       if @message.save
+        ActionCable.server.broadcast "messages_channel", {
+          id: @message.id,
+          user_id: @message.user_id,
+          text: @message.text,
+          created_at: @message.created_at,
+          user_image: @user.image_url
+        }    
         render "show.json.jb", status: :created
       else
         render json: { errors: conversation.errors.full_messages }, status: :bad_request
